594e201ac5446f5f97f890a0d9fd56eddde64076
ryo-tom <ryskdevelop@gmail.com>
2024-09-07 23:07:48 +0900

    s2: UIベース作成(WIP)

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetprofile.ui.theme.JetProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +30,104 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    // カラムで縦方向にUIを並べる
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp) // カラム全体に20dpのpadding
+                    ) {
+                        // プロフィール画像
+                        Image(
+                            painter = painterResource(id = R.drawable.img_profile),
+                            contentDescription = "プロフィール",
+                            modifier = Modifier
+                                .size(100.dp)  // 縦横サイズ
+                                .clip(RoundedCornerShape(10.dp))  // 丸角
+                        )
+                        Spacer(modifier = Modifier.height(20.dp)) // スペーサーで間隔をあけてる
+
+                        // 名前
+                        Text(
+                            text = "Ryos",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 職業
+                        Text(
+                            text = "職業: Androidエンジニア",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.fillMaxWidth(), // 外側のColumnの幅と同じにしたいため
+                        ) {
+                            // 会社名
+                            Text(
+                                text = "Rdx Company",
+                                fontSize = 26.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            
+                            // 部署・グループ名
+                            Text(
+                                text = "DXカンパニー テクノロジーグループ",
+                                color = Color.Gray,
+                                fontSize = 16.sp
+                            )
+
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            // Email
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(imageVector = Icons.Default.Email, contentDescription = "emial")
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "example@example.com", fontSize = 16.sp)
+                            Spacer(modifier = Modifier.height(5.dp))
+
+                            Divider(
+                                modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
+                                thickness = 2.dp
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+                        
+                        // 詳細表示ボタン
+                        // 文法: Buttonの最後の引数はcontentというラムダ式であり、
+                        // 最後の引数がラムダ式の時kotlinでは、かっこの外にかける
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Text(text = "詳細を表示", color = Color.White)
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 趣味 & 居住地セクション
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            // TODO: EmailのUI部分をComposableにして使い回す
+                        }
+                    }
 
                 }
             }
         }
     }
 }
+
 
