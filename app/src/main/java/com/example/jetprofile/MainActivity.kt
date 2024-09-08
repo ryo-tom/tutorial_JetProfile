b7ac49923a2150a19981123997f6627d4f2b78ef
ryo-tom <ryskdevelop@gmail.com>
2024-09-08 13:06:50 +0900

    s2: refactor: MainContentコンポーザブルに切り出し

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,60 +30,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    // カラムで縦方向にUIを並べる
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(20.dp) // カラム全体に20dpのpadding
-                    ) {
-                        // プロフィール画像
-                        Image(
-                            painter = painterResource(id = R.drawable.img_profile),
-                            contentDescription = "プロフィール",
-                            modifier = Modifier
-                                .size(100.dp)  // 縦横サイズ
-                                .clip(RoundedCornerShape(10.dp))  // 丸角
-                        )
-                        Spacer(modifier = Modifier.height(20.dp)) // スペーサーで間隔をあけてる
-
-                        // 名前
-                        Text(
-                            text = "Ryos",
-                            color = Color.Gray,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.ExtraBold,
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        // 職業
-                        Text(
-                            text = "職業: Androidエンジニア",
-                            color = Color.Gray,
-                            fontSize = 16.sp,
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        CompanySection()
-
-                        Spacer(modifier = Modifier.height(20.dp))
-                        
-                        // 詳細表示ボタン
-                        // 文法: Buttonの最後の引数はcontentというラムダ式であり、
-                        // 最後の引数がラムダ式の時kotlinでは、かっこの外にかける
-                        Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
-                            onClick = { /*TODO*/ },
-                        ) {
-                            Text(text = "詳細を表示", color = Color.White)
-                        }
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        DetailSection()
-                    }
-
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    // カラムで縦方向にUIを並べる
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(20.dp) // カラム全体に20dpのpadding
+    ) {
+        // プロフィール画像
+        Image(
+            painter = painterResource(id = R.drawable.img_profile),
+            contentDescription = "プロフィール",
+            modifier = Modifier
+                .size(100.dp)  // 縦横サイズ
+                .clip(RoundedCornerShape(10.dp))  // 丸角
+        )
+        Spacer(modifier = Modifier.height(20.dp)) // スペーサーで間隔をあけてる
+
+        // 名前
+        Text(
+            text = "Ryos",
+            color = Color.Gray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.ExtraBold,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 職業
+        Text(
+            text = "職業: Androidエンジニア",
+            color = Color.Gray,
+            fontSize = 16.sp,
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        CompanySection()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 詳細表示ボタン
+        // 文法: Buttonの最後の引数はcontentというラムダ式であり、
+        // 最後の引数がラムダ式の時kotlinでは、かっこの外にかける
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF85F6A)),
+            onClick = { /*TODO*/ },
+        ) {
+            Text(text = "詳細を表示", color = Color.White)
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        DetailSection()
     }
 }
 
